d5e32321f5de36e46b30b798b36982fa5275a5b9
Harald Pehl <harald.pehl@gmail.com>
2020-08-31 11:31:44 +0200

    Selectable card

--- src/jsMain/kotlin/org/patternfly/Card.kt
+++ src/jsMain/kotlin/org/patternfly/Card.kt
@@ -1,23 +1,29 @@
 package org.patternfly
 
+import dev.fritz2.binding.RootStore
+import dev.fritz2.binding.const
+import dev.fritz2.binding.handledBy
 import dev.fritz2.dom.html.Div
 import dev.fritz2.dom.html.HtmlElements
+import dev.fritz2.dom.html.Input
 import dev.fritz2.dom.html.TextElement
+import dev.fritz2.dom.states
+import kotlinx.coroutines.flow.map
 import org.w3c.dom.HTMLElement
 
 // ------------------------------------------------------ dsl
 
-fun HtmlElements.pfCard(classes: String? = null, content: Card.() -> Unit = {}): Card =
-    register(Card(classes), content)
+fun HtmlElements.pfCard(selectable: Boolean = false, classes: String? = null, content: Card.() -> Unit = {}): Card =
+    register(Card(selectable, classes), content)
 
-fun HtmlElements.pfCard(modifier: Modifier, content: Card.() -> Unit = {}): Card =
-    register(Card(modifier.value), content)
+fun HtmlElements.pfCard(selectable: Boolean = false, modifier: Modifier, content: Card.() -> Unit = {}): Card =
+    register(Card(selectable, modifier.value), content)
 
 fun Card.pfCardHeader(classes: String? = null, content: CardHeader.() -> Unit = {}): CardHeader =
-    register(CardHeader(classes), content)
+    register(CardHeader(this, classes), content)
 
 fun Card.pfCardHeader(modifier: Modifier, content: CardHeader.() -> Unit = {}): CardHeader =
-    register(CardHeader(modifier.value), content)
+    register(CardHeader(this, modifier.value), content)
 
 fun CardHeader.pfCardHeaderMain(classes: String? = null, content: CardHeaderMain.() -> Unit = {}): CardHeaderMain =
     register(CardHeaderMain(classes), content)
@@ -26,16 +32,22 @@ fun CardHeader.pfCardHeaderMain(modifier: Modifier, content: CardHeaderMain.() -
     register(CardHeaderMain(modifier.value), content)
 
 fun CardHeader.pfCardActions(classes: String? = null, content: CardActions.() -> Unit = {}): CardActions =
-    register(CardActions(classes), content)
+    register(CardActions(this.card, classes), content)
 
 fun CardHeader.pfCardActions(modifier: Modifier, content: CardActions.() -> Unit = {}): CardActions =
-    register(CardActions(modifier.value), content)
+    register(CardActions(this.card, modifier.value), content)
 
 fun CardHeader.pfCardTitle(classes: String? = null, content: CardTitle.() -> Unit = {}): CardTitle =
     register(CardTitle(classes), content)
 
 fun CardHeader.pfCardTitle(modifier: Modifier, content: CardTitle.() -> Unit = {}): CardTitle =
     register(CardTitle(modifier.value), content)
+
+fun CardActions.pfCardCheckbox(classes: String? = null, content: CardCheckbox.() -> Unit = {}): CardCheckbox =
+    register(CardCheckbox(this.card, classes), content)
+
+fun CardActions.pfCardCheckbox(modifier: Modifier, content: CardCheckbox.() -> Unit = {}): CardCheckbox =
+    register(CardCheckbox(this.card, modifier.value), content)
 
 fun Card.pfCardTitle(classes: String? = null, content: CardTitle.() -> Unit = {}): CardTitle =
     register(CardTitle(classes), content)
@@ -57,22 +69,63 @@ fun Card.pfCardFooter(modifier: Modifier, content: CardFooter.() -> Unit = {}): 
 
 // ------------------------------------------------------ tag
 
-class Card(classes: String?) :
+class Card(internal val selectable: Boolean, classes: String?) :
     PatternFlyComponent<HTMLElement>,
-    TextElement("article", baseClass = classes(ComponentType.Button, classes)) {
+    TextElement("article", baseClass = classes {
+        +ComponentType.Card
+        +(if (selectable) Modifier.selectable else null)
+        +classes
+    }) {
+
+    val store: CardStore = CardStore()
+
     init {
-        markAs(ComponentType.Button)
+        markAs(ComponentType.Card)
+        if (selectable) {
+            domNode.tabIndex = 0
+            classMap = store.data.map { mapOf(Modifier.selected.value to it) }
+            clicks handledBy store.flip
+        }
     }
 }
 
-class CardHeader(classes: String?) : Div(baseClass = classes("card".component("header"), classes))
+class CardHeader(internal val card: Card, classes: String?) :
+    Div(baseClass = classes("card".component("header"), classes))
 
-class CardHeaderMain(classes: String?) : Div(baseClass = classes("card".component("header", "main"), classes))
+class CardHeaderMain(classes: String?) :
+    Div(baseClass = classes("card".component("header", "main"), classes))
 
-class CardActions(classes: String?) : Div(baseClass = classes("card".component("actions"), classes))
+class CardActions(internal val card: Card, classes: String?) :
+    Div(baseClass = classes("card".component("actions"), classes)) {
+    init {
+        if (card.selectable) {
+            domNode.onclick = { it.stopPropagation() }
+        }
+    }
+}
 
-class CardTitle(classes: String?) : Div(baseClass = classes("card".component("title"), classes))
+class CardCheckbox(card: Card, classes: String?) :
+    Input(baseClass = classes) {
+    init {
+        type = const("checkbox")
+        if (card.selectable) {
+            checked = card.store.data
+            changes.states() handledBy card.store.update
+        }
+    }
+}
 
-class CardBody(classes: String?) : Div(baseClass = classes("card".component("body"), classes))
+class CardTitle(classes: String?) :
+    Div(baseClass = classes("card".component("title"), classes))
 
-class CardFooter(classes: String?) : Div(baseClass = classes("card".component("footer"), classes))
+class CardBody(classes: String?) :
+    Div(baseClass = classes("card".component("body"), classes))
+
+class CardFooter(classes: String?) :
+    Div(baseClass = classes("card".component("footer"), classes))
+
+// ------------------------------------------------------ store
+
+class CardStore : RootStore<Boolean>(false) {
+    val flip = handle { !it }
+}
