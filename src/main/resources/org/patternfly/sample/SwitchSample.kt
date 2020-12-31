5fe34ba30d0fed56fe47440a84c8922a2a3ec69f
Harald Pehl <harald.pehl@gmail.com>
2020-12-31 15:56:09 +0100

    Cleanup code

--- src/main/resources/org/patternfly/sample/SwitchSample.kt
+++ src/main/resources/org/patternfly/sample/SwitchSample.kt
@@ -2,19 +2,7 @@ package org.patternfly.sample
 
 import dev.fritz2.dom.html.render
 import dev.fritz2.dom.states
-import org.patternfly.AlertGroup
-import org.patternfly.ButtonVariation.inline
-import org.patternfly.ButtonVariation.link
 import org.patternfly.Notification
-import org.patternfly.Severity.DANGER
-import org.patternfly.Severity.INFO
-import org.patternfly.Severity.SUCCESS
-import org.patternfly.Severity.WARNING
-import org.patternfly.alert
-import org.patternfly.alertActions
-import org.patternfly.alertDescription
-import org.patternfly.alertGroup
-import org.patternfly.pushButton
 import org.patternfly.switch
 
 internal interface SwitchSample {
