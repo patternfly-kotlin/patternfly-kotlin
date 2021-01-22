050672e44c5cf882ec4138b8c4d34bd34af319b6
Harald Pehl <harald.pehl@gmail.com>
2021-01-22 10:23:03 +0100

    Fix #6: Implement avatar component

---
+++ src/main/kotlin/org/patternfly/Avatar.kt
@@ -0,0 +1,49 @@
+package org.patternfly
+
+import dev.fritz2.dom.html.Img
+import dev.fritz2.dom.html.RenderContext
+import kotlinx.coroutines.Job
+import org.w3c.dom.HTMLImageElement
+
+// ------------------------------------------------------ dsl
+
+/**
+ * Creates an [Avatar] component.
+ *
+ * @param src specifies the URL of the image for the avatar
+ * @param id the ID of the element
+ * @param baseClass optional CSS class that should be applied to the element
+ * @param content a lambda expression for setting up the component itself
+ */
+public fun RenderContext.avatar(
+    src: String,
+    id: String? = null,
+    baseClass: String? = null,
+    content: Avatar.() -> Unit = {}
+): Avatar = register(Avatar(src, id = id, baseClass = baseClass, job), content)
+
+// ------------------------------------------------------ tag
+
+/**
+ * PatternFly [avatar](https://www.patternfly.org/v4/components/avatar/design-guidelines) component.
+ *
+ * An avatar is a visual used to represent a user. It may contain an image or a placeholder graphic. Typical usage is to represent the current user in the masthead.
+ *
+ * @sample org.patternfly.sample.AvatarSample.avatar
+ */
+public class Avatar internal constructor(src: String, id: String?, baseClass: String?, job: Job) :
+    PatternFlyComponent<HTMLImageElement>,
+    Img(
+        id = id,
+        baseClass = classes {
+            +ComponentType.Avatar
+            +baseClass
+        },
+        job
+    ) {
+
+    init {
+        markAs(ComponentType.Avatar)
+        src(src)
+    }
+}
---
+++ src/main/resources/org/patternfly/sample/AvatarSample.kt
@@ -0,0 +1,13 @@
+package org.patternfly.sample
+
+import dev.fritz2.dom.html.render
+import org.patternfly.avatar
+
+internal interface AvatarSample {
+
+    fun avatar() {
+        render {
+            avatar("/assets/images/img_avatar.svg")
+        }
+    }
+}
