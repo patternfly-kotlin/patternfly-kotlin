38f0f5bc09b69b79658f19c776c1e311307f15d0
Harald Pehl <harald.pehl@gmail.com>
2021-01-27 03:15:36 +0100

    Remove deprecated gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,1 @@
 rootProject.name = "patternfly-fritz2"
-
-enableFeaturePreview("GRADLE_METADATA")
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-    }
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "kotlin2js") {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
-        }
-    }
-}
