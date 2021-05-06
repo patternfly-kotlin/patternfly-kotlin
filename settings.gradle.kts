c89ba0b855cc98d4a1c54146f95456211f3e06e3
hpehl <harald.pehl@gmail.com>
2021-05-06 11:46:28 +0200

    Introduce refreshVersions plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,17 @@
+import de.fayard.refreshVersions.bootstrapRefreshVersions
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
     }
 }
 
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies.classpath("de.fayard.refreshVersions:refreshVersions:0.9.7")
+}
+
+bootstrapRefreshVersions()
 rootProject.name = "patternfly-fritz2"
